d710744be5d7fe99386421865978839862986ca5
Amir Geva <amirgeva@gmail.com>
2021-07-10 16:35:30 +0300

    Overhaul color names

--- color.py
+++ color.py
@@ -1,3 +1,30 @@
+type_colors = {
+    "class": 16,
+    "comment": 17,
+    "concept": 18,
+    "dependent": 19,
+    "enum": 20,
+    "enumMember": 21,
+    "function": 22,
+    "macro": 23,
+    "method": 24,
+    "namespace": 25,
+    "parameter": 26,
+    "property": 27,
+    "type": 28,
+    "typeParameter": 29,
+    "variable": 30,
+}
+
+color_names = {}
+
+
+def color_name(n: int):
+    if n in color_names:
+        return color_names.get(n)
+    return f'Color {n}'
+
+
 class Color:
     TEXT = 1
     TEXT_HIGHLIGHT = 2
@@ -5,3 +32,11 @@ class Color:
     BORDER = 4
     BORDER_HIGHLIGHT = 5
     ERROR = 7
+
+
+for name in dir(Color):
+    value = getattr(Color, name)
+    if isinstance(value, int):
+        color_names[value] = name
+for name in type_colors.keys():
+    color_names[type_colors.get(name)] = name

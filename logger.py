f071e99199af9fa39efff418dbd241bd9b03d43e
Amir Geva <amirgeva@gmail.com>
2022-09-10 17:52:40 +0300

    Support optional log

--- logger.py
+++ logger.py
@@ -1,9 +1,23 @@
-Logger = open('termed.log', 'w')
+from io import TextIOWrapper
+from typing import Optional
+
+import config
+
+Logger: Optional[TextIOWrapper] = None
+
+
+def init_logger():
+    global Logger
+    if config.logging:
+        Logger = open('termed.log', 'w')
 
 
 def logwrite(s):
-    if not isinstance(s, str):
-        s = str(s)
-    Logger.write(s)
-    Logger.write('\n')
-    Logger.flush()
+    if config.logging:
+        if not Logger:
+            init_logger()
+        if not isinstance(s, str):
+            s = str(s)
+        Logger.write(s)
+        Logger.write('\n')
+        Logger.flush()
